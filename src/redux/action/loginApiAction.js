fa36cc2388ea700e3c37c475f8fae2152beebf83
Roshanyadav1 <84557981+Roshanyadav1@users.noreply.github.com>
2021-12-04 11:24:41 +0530

    removed

--- src/redux/action/loginApiAction.js
+++
@@ -1,8 +0,0 @@
-const LOGIN_CALL = 'LOGIN_CALL'
-
-export const loginApiCall =(userData)=> {
-    return{
-        type:LOGIN_CALL,
-        payload:userData
-    }
-}
--- src/redux/action/rootAction.js
+++
@@ -1,1 +0,0 @@
-// this file for export all the acton from action folder 
