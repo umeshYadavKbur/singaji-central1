ef5d423792e3c4f6491170546696beda7bebfc73
Roshanyadav1 <84557981+Roshanyadav1@users.noreply.github.com>
2021-12-04 12:36:25 +0530

    handled_api 404 error still remaining to solve

--- src/redux/actionDispatcher/authDispatcher.js
+++ src/redux/actionDispatcher/authDispatcher.js
@@ -1,58 +1,52 @@
-import { LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS } from '../constants/actions'
-import getData from '../../services/agent'
+import { LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS } from "../constants/actions";
+import getData from "../../services/agent";
 
 export const fetchUsers = (data) => {
   return async (dispatch) => {
-
     // Sending the additional url to be attached on baseUrl in other function
-    const loginUrl = "/api/login"
+    const loginUrl = "/api/login";
 
     // wait untill the data not received so getData function take data and url part
-    var userResData = await getData(data, loginUrl)
+    dispatch(loginRequest());
+    var userResData = await getData(data, loginUrl);
     // Printing the data is coming or not
     // console.log("Working  :::: ", userResData);
     try {
       if (userResData.token) {
         //setting the Items in localStorage
-        localStorage.setItem('user', userResData.user);
-        localStorage.setItem('token', userResData.token);
-        localStorage.setItem('role', userResData.role);
+        localStorage.setItem("user", userResData.user);
+        localStorage.setItem("token", userResData.token);
+        localStorage.setItem("role", userResData.role);
         //Redirect to the home page remaining
         // history.push('/');
-
         //dispatch action and store data in it
-        dispatch({
-          type: LOGIN_SUCCESS,
-          payload: userResData,
-        })
+        dispatch(loginSuccess(userResData));
+      } else {
+        dispatch(loginFailure(userResData));
       }
     } catch (error) {
       //if crudential fails than Login fail action dispatch
-      dispatch({
-        type: LOGIN_FAIL,
-        payload: error,
-      })
+      dispatch(loginFailure(error));
     }
-  }
-}
-
+  };
+};
 
 export const loginRequest = () => {
   return {
-    type: LOGIN_REQUEST
-  }
-}
+    type: LOGIN_REQUEST,
+  };
+};
 
-export const loginSuccess = users => {
+export const loginSuccess = (users) => {
   return {
     type: LOGIN_SUCCESS,
-    payload: users
-  }
-}
+    payload: users,
+  };
+};
 
-export const loginFailure = error => {
+export const loginFailure = (error) => {
   return {
     type: LOGIN_FAIL,
-    payload: error
-  }
-}
+    payload: error,
+  };
+};
