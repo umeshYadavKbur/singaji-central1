0942d133c976e0cbe6a2f184cf76a25573fef4c0
HarshDixit <harshdixit991@gmail.com>
2021-12-06 12:40:23 +0530

    index.js updated

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,4 @@
 //export all the dispatcher at one place so whenever the function are not exported error generate so reimport it once more
 export * from './actionDispatcher/authDispatcher.js'
 export * from './actionDispatcher/newPassDispatcher.js'
+export * from './actionDispatcher/forgotpassDispatcher.js'
