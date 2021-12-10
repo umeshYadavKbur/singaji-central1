6a90e1c6387409ffe1ffdadbb5692d9c115462e4
Roshanyadav1 <roshany.bca2020@ssism.org>
2021-12-10 20:38:44 +0530

    Added the profile  pic navigation and popup

--- src/components/superAdmin/dashbord/components/Navbar.js
+++ src/components/superAdmin/dashbord/components/Navbar.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import React, { useState } from "react";
 import "../styles/Navbar.css";
-import logo from "../../../assests/image/Mask Group 39.png";
 import back_btn from "../../../assests/image/back_btn.svg";
+import AppHeaderDropdown from "./HeaderDropdown";
 
 function Navbar() {
-  // const [box, setBox] = useState(false);
   return (
     <div className="container_navbar">
       <div className="navbar_container_start_side">
@@ -13,12 +11,7 @@ function Navbar() {
         <h3 style={{ color: "#5A607F", marginLeft: "10px" }}>Dashboard</h3>
       </div>
       <div className="navbar_container_end_side">
-        <img
-          src={logo}
-          alt="Admin"
-          style={{ height: "50px", width: "50px", cursor: "pointer" }}
-          // onMouseEnter={() => setBox(true)}
-          // onMouseLeave={() => setBox(false)}
+        <AppHeaderDropdown
         />
       </div>
     </div>
