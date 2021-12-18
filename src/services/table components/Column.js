19b6ba96c672fd23209f914899f383c4056ec671
HarshDixit <harshdixit991@gmail.com>
2021-12-18 16:28:04 +0530

    button added on status api handel remain

--- src/services/table components/Column.js
+++ src/services/table components/Column.js
@@ -20,39 +20,39 @@
         header: 'Role',
         accessor: 'roleId'
     }
-    // ,
-    //  {
-    //      header: 'Status',
-    //      accessor: 'status',
-    //      Cell: ({row: {original}}) => (
-    //          <button
-    //              style={
-    //                  original.status === "Active"
-    //                      ? {
-    //                          width: "80px",
-    //                          borderRadius: "5px",
-    //                          backgroundColor: "#FFC700",
-    //                          color: "white",
-    //                          fontWeight: "bold",
-    //                          border: '1px #FFC700'
-    //                      }
-    //                      : {
-    //                          width: "80px",
-    //                          backgroundColor: "#FBC775",
-    //                          borderRadius: "5px",
-    //                          fontWeight: "bold",
-    //                          color: "white",
-    //                          border: 'none'
-    //                      }}
-    //              onClick={() => {
-    //                  // setData(original.status)
-    //                  console.log(original)
-    //                  alert("Do you want to change status of  : " + original.firstName)
-    //              }}>
-    //              {original.status}
-    //          </button>
-    //      )
-    //  }
+    ,
+     {
+         header: 'Status',
+         accessor: 'status',
+         Cell: ({row: {original}}) => (
+             <button
+                 style={
+                     original.status === "Active"
+                         ? {
+                             width: "80px",
+                             borderRadius: "5px",
+                             backgroundColor: "#FFC700",
+                             color: "white",
+                             fontWeight: "bold",
+                             border: '1px #FFC700'
+                         }
+                         : {
+                             width: "80px",
+                             backgroundColor: "#FBC775",
+                             borderRadius: "5px",
+                             fontWeight: "bold",
+                             color: "white",
+                             border: 'none'
+                         }}
+                 onClick={() => {
+                     // setData(original.status)
+                     console.log(original)
+                     alert("Do you want to change status of  : " + original.firstName)
+                 }}>
+                 {original.status}
+             </button>
+         )
+     }
 
 ]
 
