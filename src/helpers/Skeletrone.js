acab075634ef7b8f0196b17ee8ee4c78da6d7163
kuldeep <kuldeeppanwar7697@gmail.com>
2021-12-21 21:38:41 +0530

    skelaton change

--- src/helpers/Skeletrone.js
+++ src/helpers/Skeletrone.js
@@ -1,14 +1,69 @@
 import * as React from 'react';
 import Skeleton from '@mui/material/Skeleton';
+import { array } from 'yup/lib/locale';
 
 export default function SkeletonColor() {
+    let arr = [1, 1, 1, 1, 1, 1, 1];
     return (
-       
-            <Skeleton
-            sx={{bgcolor: "rgb(190, 190, 190)"}}
+        <>
+            <div className='d-flex justify-content-between'>
+
+
+                <Skeleton className='ml-4'
+                    sx={{ bgcolor: "rgb(190, 190, 190)" }}
+                    variant="rectangular"
+                    style={{ height: "30px", width: "120px", borderRadius: '3px' }}
+                />
+
+
+
+
+                <Skeleton className='mr-5'
+                    sx={{ bgcolor: "rgb(190, 190, 190)" }}
+                    variant="rectangular"
+                    style={{ height: "30px", width: "200px", borderRadius: '3px' }}
+                />
+            </div>
+            <Skeleton className='ml-3 '
+                sx={{ bgcolor: "rgb(190, 190, 190)" }}
                 variant="rectangular"
-                style={{width:'95%',height:"95%"}}
+                style={{ width: '97%', height: "7%", borderRadius: '3px', marginTop: '15px' }}
             />
-       
+            {
+                arr.map((e, i) => {
+
+                    return (
+                        <Skeleton key={i} className='ml-3 mt-3'
+                            sx={{ bgcolor: "rgb(190, 190, 190)" }}
+                            variant="rectangular"
+                            style={{ width: '97%', height: "7%", borderRadius: '3px' }}
+                        />
+                    )
+                })
+
+
+            }
+            <div className='d-flex justify-content-between'>
+
+
+                <Skeleton className='ml-4 mt-3'
+                    sx={{ bgcolor: "rgb(190, 190, 190)" }}
+                    variant="rectangular"
+                    style={{ height: "25px", width: "200px", borderRadius: '3px' }}
+                />
+
+
+
+
+                <Skeleton className='mr-3 mt-3'
+                    sx={{ bgcolor: "rgb(190, 190, 190)" }}
+                    variant="rectangular"
+                    style={{ height: "30px", width: "200px", borderRadius: '3px' }}
+                />
+            </div>
+
+
+
+        </>
     );
 }
