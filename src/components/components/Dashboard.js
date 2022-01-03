2557f856024b0679575b847d9cd1ebc10db89f1c
goutam <www.gautammalviya3@gmail.com>
2022-01-03 07:39:39 +0530

    ui changes

--- src/components/components/Dashboard.js
+++ src/components/components/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div className="position-absolute top-50 start-50 translate-middle text-secondary fw-bolder">
+    <div className="position-absolute top-50 start-50 translate-middle  fw-bolder">
       <h1 className="fw-bold " style={{color: '#5A607F'}}>Coming Soon...</h1>
     </div>
   );
