07b94d2211c736dcc28fd387cae922aeed3b0772
kuldeep <kuldeeppanwar7697@gmail.com>
2022-01-06 22:12:58 +0530

    ok

--- src/components/components/accounts/UploadDocumentFrom.js
+++ src/components/components/accounts/UploadDocumentFrom.js
@@ -32,7 +32,7 @@ function UploadDocumentFrom() {
 
     const imageToBase64 = (file, feildName) => {
 
-      
+
         var reader = new FileReader();
         if (file) {
             reader.readAsDataURL(file)
@@ -58,58 +58,37 @@ function UploadDocumentFrom() {
         }
     }
 
+    const ImgDiv = ({ IMG, DISPLAY_NAME, NAME_FOR_FILE }) => {
+        return (
+            <div className="col m-2" style={{ height: '250px', width: 'auto', backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
+                {IMG === '' ? <div style={{ height: '190px', width: '100%' }} className='d-flex justify-content-center' ><img className='mx-auto my-auto' height='80px' src={UploadDocumentImage} alt="avtar_photo" /></div> : <img className='mt-1 p-1' src={IMG} alt="avtar_photo2" style={{ height: '190px', width: '100%' }} />}
+                <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2 " style={{ width: '90%' }}
+                    onClick={() => { document.getElementById(NAME_FOR_FILE).click() }}>
+                    {DISPLAY_NAME}</button>
+                <input name={NAME_FOR_FILE} id={NAME_FOR_FILE} type="file" onChange={(e) => {
+                    imageToBase64(e.target.files[0], NAME_FOR_FILE);
+                }} value={formik.values.NAME_FOR_FILE} style={{ display: 'none' }} />
+            </div>
+        )
+    }
+
     return (
         <>
             <form onSubmit={formik.handleSubmit}>
                 <div className="row m-1">
-                    <div className="col m-2" style={{ height: '250px', width: 'auto', backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        {imgData.passportPhoto === '' ? <div style={{ height: '190px', width: '100%' }} ><img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" /></div> : <img className='mt-1 p-1' src={imgData.passportPhoto} alt="avtar_photo2" style={{ height: '190px', width: '100%' }} />}
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2 " style={{ width: '90%' }}
-                            onClick={() => { document.getElementById("passportPhoto").click() }}>
-                            + Password Photo</button>
-                        <input name='passportPhoto' id='passportPhoto' type="file" onChange={(e) => {
-                            imageToBase64(e.target.files[0], 'passportPhoto');
-                        }} value={formik.values.passportPhoto} style={{ display: 'none' }} />
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ 10<sup>th</sup> Marksheet</button>
-                    </div>
+                    {<ImgDiv IMG={imgData.passportPhoto} DISPLAY_NAME={'+ Passport Photo'} NAME_FOR_FILE={'passportPhoto'} />}
+                    {<ImgDiv IMG={imgData.marksheet10} DISPLAY_NAME={'+ 10th Marksheet'} NAME_FOR_FILE={'marksheet10'} />}
+                    {<ImgDiv IMG={imgData.marksheet12} DISPLAY_NAME={'+ 12th Marksheet'} NAME_FOR_FILE={'marksheet12'} />}
+                    {<ImgDiv IMG={imgData.aadharCard} DISPLAY_NAME={'+ Aadhar Card'} NAME_FOR_FILE={'aadharCard'} />}
+                    {<ImgDiv IMG={imgData.castCertificate} DISPLAY_NAME={'+ Cast Certificate'} NAME_FOR_FILE={'castCertificate'} />}
 
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ 12<sup>th</sup> Marksheet</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Aadhar</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Cast Certificate</button>
-                    </div>
                 </div>
                 <div className="row m-1">
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Income Cretificate</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Mp Domicile</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Transfer Certificate</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Other Document</button>
-                    </div>
-                    <div className="col m-2" style={{ backgroundColor: 'white', border: '3px solid #7E84A3', borderRadius: '5px' }}>
-                        <img className='mt-1 p-1' src={UploadDocumentImage} alt="avtar_photo" style={{ height: '190px', width: '100%' }} />
-                        <button type="button" className="btn btn-sm btn-outline-warning fw-bold m-2" style={{ width: '90%' }}>+ Other Document</button>
-                    </div>
+                    {<ImgDiv IMG={imgData.incomeCertificate} DISPLAY_NAME={'+ Income Cretificate'} NAME_FOR_FILE={'incomeCertificate'} />}
+                    {<ImgDiv IMG={imgData.domicile} DISPLAY_NAME={'+ Mp Domicile'} NAME_FOR_FILE={'domicile'} />}
+                    {<ImgDiv IMG={imgData.transferCertificate} DISPLAY_NAME={'+ Transfer Certificate'} NAME_FOR_FILE={'transferCertificate'} />}
+                    {<ImgDiv IMG={imgData.otherDocument1} DISPLAY_NAME={'+ Other Document'} NAME_FOR_FILE={'otherDocument1'} />}
+                    {<ImgDiv IMG={imgData.otherDocument2} DISPLAY_NAME={'+ Other Document'} NAME_FOR_FILE={'otherDocument2'} />}
                 </div>
 
                 <div className="justify-self-end mb=5">
