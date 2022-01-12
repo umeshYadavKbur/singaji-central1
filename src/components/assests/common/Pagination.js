2fff54f630d05990a67aec7796c914dd3be3e60c
Roshanyadav1 <84557981+Roshanyadav1@users.noreply.github.com>
2022-01-12 10:18:17 +0530

    pagination done with styles

--- src/components/assests/common/Pagination.js
+++ src/components/assests/common/Pagination.js
@@ -15,9 +15,9 @@ function Pagination(
     }
 ) {
     return (
-        <div style={{ border: "rgb(246 249 252)" }} className="d-flex mb-4">
+        <div style={{ border: "rgb(246 249 252)" }} className="d-flex">
             <div className="mx-4">
-                <span style={{ fontWeight: '700', color: "gray", }}>
+                <span style={{ fontWeight: '300', color: "#302E2F", }}>
                     Showing {page.length * (pageIndex + 1) - (page.length - 1)} to{" "}
                     {page.length * (pageIndex + 1)} of {pageCount * pageSize} Entries{" "}
                     {"  "}
@@ -26,11 +26,7 @@ function Pagination(
             <div className="ml-auto me-3">
                 {/* <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{"<<"}</button> */}
                 <button
-                    style={canPreviousPage ?
-                        { height: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid gray", borderRadius: " 10px 0px 0px 10px" }
-                        : {
-                            backgroundColor: "#bbbbbb", height: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid white", borderRadius: " 10px 0px 0px 10px "
-                        }}
+                    style={{ height: "28px", color: "#302E2F", fontWeight: '300', outline: "none", border: "1px solid gray", borderRadius: " 4px 0px 0px 4px" }}
                     onClick={() => previousPage()}
                     disabled={!canPreviousPage}
                 >
@@ -38,20 +34,17 @@ function Pagination(
                 </button>
                 <button
                     style={{
-                        height: "35px", width: "30px",
-                        color: "gray",
-                        fontWeight: '700', outline: "none", border: "2px solid gray", color: 'white', backgroundColor: '#898989'
+                        height: "30px", width: "28px",
+                        color: "#302E2F",
+                        fontWeight: '300', outline: "none", border: "1px solid gray", color: '#302E2F', backgroundColor: '#898989'
                     }}
                 >
                     {pageIndex + 1}
                 </button>
                 <button
                     style={
-                        pageOptions.length > pageIndex + 1 ?
-                            { height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid gray" }
-                            : {
-                                backgroundColor: "#bbbbbb", height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid white"
-                            }
+                        { height: "28px", width: "28px", color: "#302E2F", fontWeight: '300', outline: "none", border: "1px solid gray" }
+
                     }
                     onClick={() => gotoPage(pageIndex + 1)}
                     disabled={pageOptions.length < pageIndex + 2}
@@ -61,11 +54,8 @@ function Pagination(
                 <button
                     disabled={pageOptions.length < pageIndex + 3}
                     style={
-                        pageOptions.length > pageIndex + 2 ?
-                            { height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid gray" }
-                            : {
-                                backgroundColor: "#bbbbbb", height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid white"
-                            }}
+                        { height: "28px", width: "28px", color: "#302E2F", fontWeight: '300', outline: "none", border: "1px solid gray" }
+                    }
                     onClick={() => {
                         gotoPage(pageIndex + 2);
                     }}
@@ -73,23 +63,14 @@ function Pagination(
                     {pageIndex + 3}
                 </button>
                 <button
-                    style={
-                        pageOptions.length > pageIndex + 3 ?
-                            { height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid gray" }
-                            : {
-                                backgroundColor: "#bbbbbb", height: "31px", width: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid white"
-                            }}
+                    style={{ height: "28px", width: "28px", color: "#302E2F", fontWeight: '300', outline: "none", border: "1px solid gray" }}
                     onClick={() => gotoPage(pageIndex + 3)}
                     disabled={pageOptions.length < pageIndex + 4}
                 >
                     {pageIndex + 4}
                 </button>
                 <button
-                    style={canNextPage ?
-                        { height: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid gray", borderRadius: "0px 10px 10px 0px" }
-                        : {
-                            backgroundColor: "#bbbbbb", height: "31px", color: "gray", fontWeight: '700', outline: "none", border: "2px solid white", borderRadius: "0px 10px 10px 0px"
-                        }}
+                    style={{ height: "28px", color: "#302E2F", fontWeight: '300', outline: "none", border: "1px solid gray", borderRadius: "0px 4px 4px 0px" }}
                     onClick={() => nextPage()}
                     disabled={!canNextPage}
                 >
