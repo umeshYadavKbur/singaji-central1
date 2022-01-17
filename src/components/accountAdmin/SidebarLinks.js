3c6184b01ebdcd7f6a36e16ed56d1b31f1c148cd
kuldeep panwar <kuldeeppanwar7697@gmail.com>
2022-01-17 22:08:12 +0530

    side fix

--- src/components/accountAdmin/SidebarLinks.js
+++ src/components/accountAdmin/SidebarLinks.js
@@ -1,12 +1,12 @@
-import React, { useState,useEffect } from 'react';
-import { Link, NavLink,useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink, useLocation } from "react-router-dom";
 // import Dashboard_svg from "../assests/image/sidebarIcons/Dashboard.svg";
-import Alumini_svg from "../assests/image/sidebarIcons/Alumini.svg";
+// import Alumini_svg from "../assests/image/sidebarIcons/Alumini.svg";
 import Accounts_svg from "../assests/image/sidebarIcons/Accounts.svg";
-import External_company_svg from "../assests/image/sidebarIcons/External_company.svg";
-import Donation_svg from "../assests/image/sidebarIcons/Donation.svg";
-// import Education_svg from "../assests/image/sidebarIcons/Education.svg";
-import Others_svg from "../assests/image/sidebarIcons/Others.svg";
+// import External_company_svg from "../assests/image/sidebarIcons/External_company.svg";
+// import Donation_svg from "../assests/image/sidebarIcons/Donation.svg";
+// // import Education_svg from "../assests/image/sidebarIcons/Education.svg";
+// import Others_svg from "../assests/image/sidebarIcons/Others.svg";
 
 const SidebarLinks = ({ Toggle }) => {
     const [active_dropdown, setActiveDropdown] = useState('Dashboard');
@@ -17,15 +17,15 @@ const SidebarLinks = ({ Toggle }) => {
     // var location = getLocation();
     const base = '/account_admin_dashboard'
 
-    const changeTab = (id) => {
+    // const changeTab = (id) => {
 
-        setActiveDropdown('');
-        setActiveTab(id);
-        // setActiveMenu({})
+    //     setActiveDropdown('');
+    //     setActiveTab(id);
+    //     // setActiveMenu({})
 
-    }
+    // }
 
-    const DLink1 = (currentLocation,name, url, icon, id, parentId) => {
+    const DLink1 = (currentLocation, name, url, icon, id, parentId) => {
         useEffect(() => {
             const changeTab1 = () => {
 
@@ -42,10 +42,28 @@ const SidebarLinks = ({ Toggle }) => {
         }, [currentLocation, id, parentId, url])
         return (
             <NavLink
+
                 to={url}
                 id={id}
                 className={`sidebar_options_link ${active_dropdown === id ? "sidebar_options_active" : ""}`}
-                onClick={() => { setActiveDropdown(id); setActiveTab(parentId) }}
+                onClick={(e) => {
+                    if (Toggle) {
+
+                        // document.getElementById('dashboard-collapse').classList.remove('show');
+                        // document.getElementById('education-collapse').classList.remove('show');
+                        document.getElementById('account-collapse').classList.remove('show');
+                        setActiveMenu((pre) => {
+
+                            return {
+                                dashboard: false,
+                                account: false,
+                                education: false
+                            }
+                        })
+                    }
+                    setActiveDropdown(id);
+                    setActiveTab(parentId)
+                }}
             >
                 <img src={icon} className=" Sidebar_text sidebar_icons" alt="" />
                 <span className="text-dark ">{name}</span>
@@ -56,12 +74,20 @@ const SidebarLinks = ({ Toggle }) => {
     return (
         <>
             {/* ---- third dropdown---- */}
+          
+
             <div className="flex-shrink-0">
                 <ul className="list-unstyled m-0">
                     <a
                         onClick={() => { setActiveMenu((pre) => { return { ...pre, account: !active_menu.account } }) }}
                         href="#!"
-                        style={{ borderBottomRightRadius: `${active_menu.account === true ? '0px' : ''}` }}
+                        // style={{ borderBottomRightRadius: `${active_menu.account === true ? '0px' : ''}` }}
+                        style={Toggle ?
+                            !active_menu.account ?
+                                { width: '60px', borderBottomRightRadius: `${active_menu.account === true ? '0px' : ''}` }
+                                :
+                                { borderBottomRightRadius: `${active_menu.account === true ? '0px' : ''}` }
+                            : { borderBottomRightRadius: `${active_menu.account === true ? '0px' : ''}` }}
                         className={`data-toggle sidebar_options_drop d-flex justify-content-between ${active_tab === 'account-collapse-btn' ? 'active_tab' : ''} ${active_menu.account === true ? 'active_tab' : ''} `}
                         role="button"
                         // data-toggle="collapse"
@@ -74,7 +100,11 @@ const SidebarLinks = ({ Toggle }) => {
                                 className="  Sidebar_text  sidebar_icons"
                                 alt=""
                             />
-                            <span className="text-dark ">Account</span>
+                            <span
+                                style={
+                                    Toggle ?
+                                        !active_menu.account ? { display: 'none' } :
+                                            {} : {}} className="text-dark ">Account</span>
                         </div>
                         {!Toggle && <i className="fas fa-chevron-down mr-3"></i>}
                     </a>
@@ -82,15 +112,14 @@ const SidebarLinks = ({ Toggle }) => {
                     <div className="collapse collapse_superadmin" id="account-collapse">
                         <ul className={`${Toggle ? 'btn-toggle-nav-hide' : 'btn-toggle-nav'}  list-unstyled `}>
                             <li className="dropdown_items_div ml-1">
-                                {DLink1(currentLocation,"Student Account", "studentaccounttable", "ok", "Student_account", 'account-collapse-btn')}
-                                {DLink1(currentLocation,"Pending Scholarship", "pendingscholarship", "ok", "Pending_students", "account-collapse-btn")}
-                                {DLink1(currentLocation,"Fees Receipt List", "feesreceiptlist", "ok", "Fees_receiptlist", 'account-collapse-btn')}
+                                {DLink1(currentLocation, "Student Account", "studentaccounttable", "ok", "Student_account", 'account-collapse-btn')}
+                                {DLink1(currentLocation, "Pending Scholarship", "pendingscholarship", "ok", "Pending_students", "account-collapse-btn")}
+                                {DLink1(currentLocation, "Fees Receipt List", "feesreceiptlist", "ok", "Fees_receiptlist", 'account-collapse-btn')}
                             </li>
                         </ul>
                     </div>
                 </ul>
             </div>
-
             {/* Thirt dropdown_items_div
             <Link className={`sidebar_options ${active_tab === 'Accounts' ? 'active_tab' : ''}`}
                 to="studentaccounttable"
@@ -109,54 +138,7 @@ const SidebarLinks = ({ Toggle }) => {
 
 
 
-            <Link className={`sidebar_options ${active_tab === 'Alumini' ? 'active_tab' : ''}`} to="#!"
-
-                id="Alumini"
-                onClick={() => { changeTab("Alumini") }}
-            >
-                <img
-                    src={Alumini_svg}
-                    className=" Sidebar_text  sidebar_icons"
-                    alt=""
-                />
-                <span className="text-dark">Alumini</span>
-            </Link>
-            <Link className={`sidebar_options ${active_tab === 'External_Companies' ? 'active_tab' : ''}`} to="#!"
-
-                id='External_Companies'
-                onClick={() => { changeTab("External_Companies") }}
-            >
-                <img
-                    src={External_company_svg}
-                    className=" Sidebar_text  sidebar_icons"
-                    alt=""
-                />
-                <span className="text-dark">External Companies</span>
-            </Link>
-            <Link className={`sidebar_options ${active_tab === 'donation' ? 'active_tab' : ''}`} to="#!"
-
-                id='donation'
-                onClick={() => { changeTab("donation") }}
-            >
-                <img
-                    src={Donation_svg}
-                    className=" Sidebar_text  sidebar_icons"
-                    alt=""
-                />
-                <span className="text-dark">Donation</span>
-            </Link>
-            <Link className={`sidebar_options ${active_tab === 'others' ? 'active_tab' : ''}`} to="#"
-
-                id='others'
-                onClick={() => { changeTab("others") }}
-            >
-                <img
-                    src={Others_svg}
-                    className=" Sidebar_text  sidebar_icons"
-                    alt=""
-                />
-                <span className="text-dark">Others</span>
-            </Link>
+        
         </>
     );
 };
