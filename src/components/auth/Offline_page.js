57d6c0d31547bc450868c6a01696786c7e4ba743
goutam <www.gautammalviya3@gmail.com>
2022-01-19 13:32:43 +0530

    added offline page

--- src/components/auth/Offline_page.js
+++ src/components/auth/Offline_page.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Offline_svg from '../assests/image/Offline.svg';
+import "./Styles/Offline_page.css"
 
 const Offline_page = () => {
     return (
         <>
-            <div className="container d-flex justify-content-center" style={{ backgroundColor: "blueviolet"}}>
-                <img src={Offline_svg} alt="you are offline!" />
+            <div className=" container-fluid main-div">
+                <div className="row">
+                <img src={Offline_svg} className='offline-svg' alt="you are offline!" />
+                </div>
+                <div className=" row offline-text">Slow or no Internet connection</div>
             </div>
         </>
     )
