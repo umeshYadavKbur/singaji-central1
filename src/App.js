069b2442ae3f8a0b8bd5988dfa05338ae881d732
goutam <www.gautammalviya3@gmail.com>
2022-01-20 11:17:20 +0530

    *

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
             <Routes>
 
 
-              <Route exact path="/login" element={<Offline />} />
+              <Route exact path="/login" element={<Login />} />
               <Route exact path="/forgetpassword" element={<ForgotPassword />} />
               <Route
                 exact
