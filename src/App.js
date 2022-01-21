1c631ab448c35b3a87e1e49f2add0312129956f5
Roshanyadav1 <84557981+Roshanyadav1@users.noreply.github.com>
2022-01-21 14:05:22 +0530

    merging gouthamNew to roshn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  // useNavigate,
 } from "react-router-dom";
 // import { useParams } from 'react-router';
 // import { Redirect } from "react-router";
@@ -26,45 +27,77 @@ import StudentAdmin from "./helpers/StudentAdmin";
 
 
 
-// import Offline from '../src/components/auth/OfflinePage';
+import Offline from '../src/components/auth/OfflinePage';
 
 function App() {
+  const [is_online, setIs_online] = useState(true)
   // const navigate = useNavigate()
-  // window.addEventListener("online", function() {
-  //   console.log("internet is ok!👍")
-  // });
+  window.addEventListener("online", function () {
+    console.log("internet is ok!👍")
+    setIs_online(true)
+  });
 
-  // window.addEventListener("offline", function() {
-  //   alert("Oops! You are offline now!");
-  //   // navigate("/offline")
-  //   //  return  <Offline></Offline>
-  // });
+  window.addEventListener("offline", function () {
+    // alert("Oops! You are offline now!");
+    // navigate("/offline")
+    setIs_online(false)
+    //  return  <Offline></Offline>
+  });
+
+  //   function updateConnectionStatus() {  
+  //     if(navigator.onLine) {
+  //       console.log("......................................");
+  //       alert("online");
+
+  //     } else {
+  //       alert("ofline")
+
+  //     }
+  // }
+  // updateConnectionStatus();
+
+  // var isOnline = window.navigator.onLine;
+  // if (isOnline) {
+  //   alert('online');
+  // } else {
+  //   alert('offline');
+  // }
 
 
 
   return (
-
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/forgetpassword" element={<ForgotPassword />} />
-          <Route
-            exact
-            path="/create_new_password/:token"
-            element={<ResetPassword />}
-          />
-          <Route element={<SuperAdmin />}>
-            <Route path="/admin_dashboard/*" element={<AdminDashboard />} />
-          </Route>
-          <Route element={<AccountAdmin />}>
-            <Route path="/account_admin_dashboard/*" element={<AccountAdminDashboard />} />
-          </Route>
-          <Route element={<StudentAdmin />}>
-            <Route path="/student_admin_dashboard/*" element={<StudentAdminDashboard />} />
-          </Route>
-          <Route path="*" element={<Navigate replace to="/login" />} />
-        </Routes>
+        {
+          is_online ?
+            <Routes>
+
+
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/forgetpassword" element={<ForgotPassword />} />
+              <Route
+                exact
+                path="/create_new_password/:token"
+                element={<ResetPassword />}
+              />
+              <Route element={<SuperAdmin />}>
+                <Route path="/admin_dashboard/*" element={<AdminDashboard />} />
+              </Route>
+              <Route element={<AccountAdmin />}>
+                <Route path="/account_admin_dashboard/*" element={<AccountAdminDashboard />} />
+              </Route>
+              <Route element={<StudentAdmin />}>
+                <Route path="/student_admin_dashboard/*" element={<StudentAdminDashboard />} />
+              </Route>
+              <Route path="*" element={<Navigate replace to="/login" />} />
+            </Routes>
+            :
+
+            <Routes>
+
+              <Route exact path="*" element={<Offline />} />
+            </Routes>
+        }
       </Router>
     </Provider>
   );
--- src/components/auth/OfflinePage.js
+++ src/components/auth/OfflinePage.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import Offline_svg from '../assests/image/Offline.svg';
+import React, { useEffect, useState } from 'react';
+import Offline_svg from '../assests/image/error2.png';
+import "./Styles/Offline_page.css"
 
 const OfflinePage = () => {
+
+    // onClick={window.location.reload()}
+    // function refreshPage(){
+    //     window.location.reload();
+    // } 
+    // onClick={refreshPage()}
+
+    const [sendRequest, setSendRequest] = useState(false);
+
+    useEffect(() => {
+        if (sendRequest) {
+            //send the request
+            //    setSendRequest(false);
+            window.location.reload();
+            console.log("page reloaded 👍!")
+        }
+    },
+        [sendRequest]);
+
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%' }}>
-                <div >
-                    <img src={Offline_svg} alt="you are offline!" />
+            <div className="d-flex justify-content-center align-items-center main-div">
+                <div>
+
+                    <div className="">
+                        <img src={Offline_svg} className='offline-svg' alt="you are offline!" />
+                    </div>
+
+                    <div className='offlinePage_reload' >
+                        <div className=" row offline-text">No internet connection found! <br />check your connection</div>
+                        <button onClick={() => setSendRequest(true)} className='Offline-page-button' >Reload</button>
+                    </div>
                 </div>
             </div>
         </>
